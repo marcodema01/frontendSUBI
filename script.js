5b3ea08a103b832511f44d87c7ff710f0f62eb49
Marco De Martino <marco.dema2001@gmail.com>
2025-06-18 16:04:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// fetch("http://localhost:3001/insert-characters", { method: "POST" })
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log("Risposta inserimento:", data);
+//     // Dopo l'inserimento, puoi leggere i dati aggiornati
+//     return fetch("http://localhost:3001/characters");
+//   })
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log("Dati ricevuti dal backend:", data);
+//     // ...gestisci i dati...
+//   })
+//   .catch((err) => {
+//     console.error("Errore dal backend:", err);
+//   });
+
+document.getElementById("btn1").addEventListener("click", () => {
+  aggiornaDato("1");
+});
+document.getElementById("btn2").addEventListener("click", () => {
+  aggiornaDato("2");
+});
+document.getElementById("btn3").addEventListener("click", () => {
+  aggiornaDato("3");
+});
+
+function aggiornaDato(id) {
+  fetch("http://localhost:3001/update-character", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ id }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Risposta update:", data);
+      return fetch("http://localhost:3001/characters");
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Dati ricevuti dal backend:", data);
+      // ...gestisci i dati...
+    })
+    .catch((err) => {
+      console.error("Errore dal backend:", err);
+    });
+}

dc6e075e0f8157c13f88b913bfae22f1ec1f3819
Marco De Martino <marco.dema2001@gmail.com>
2025-06-19 11:48:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.getElementById("btn3").addEventListener("click", () => {
 });
 
 function aggiornaDato(id) {
-  fetch("http://localhost:3001/update-character", {
+  // fetch("http://localhost:3001/update-character", {
   fetch("https://subi-ajng.onrender.com/update-character", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -34,7 +34,7 @@ function aggiornaDato(id) {
     .then((res) => res.json())
     .then((data) => {
       console.log("Risposta update:", data);
-      return fetch("http://localhost:3001/characters");
+      // return fetch("http://localhost:3001/characters");
       return fetch("https://subi-ajng.onrender.com/characters");
     })
     .then((res) => res.json())
